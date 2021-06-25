5fa2c413e92ef1e3bd625dc52fefdedf25f6ef9b
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-06-25 21:30:29 +0530

    Added SSG

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -10,4 +10,44 @@ const MeetupDetails = () => {
   );
 };
 
+// The dynamic ids for which the page needs to be pre rendered should be specified
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://img.theculturetrip.com/x/smart/wp-content/uploads/2019/04/shutterstock_421013719.jpg",
+        id: id,
+        title: "First Adventure",
+        address: "Brazil",
+        description: "Fun Adventure",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
